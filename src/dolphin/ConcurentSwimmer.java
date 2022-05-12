e81a6289471ac962c03373e3485a672cd431dbe5
DatJustino <100353161+DatJustino@users.noreply.github.com>
2022-05-12 13:01:17 +0200

    Readfromfile working adding more users

--- src/dolphin/ConcurentSwimmer.java
+++ src/dolphin/ConcurentSwimmer.java
@@ -1,4 +1,31 @@
 package dolphin;
 
-public class ConcurentSwimmer {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ConcurentSwimmer extends User {
+  Iterator<User> it = getListAllUsers().iterator();
+  User user = new User();
+  Admin admin = new Admin();
+  Controller control = new Controller();
+  ArrayList<User> listAllUsers = new ArrayList<>();
+
+  public void findingMembers() {
+    for (int i = 0; i < getListAllUsers().size(); i++ ) {
+      System.out.println(getListAllUsers().get(i).toString());
+    }
+    System.out.println("yooo");
+   getListAllUsers().toString();
+
+    System.out.println(getListAllUsers().toString().trim());
+
+  }
+
 }
+    // If member is "COMPETITOR", add read from list. (Maybe add to new list.)
+
+
